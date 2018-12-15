1feaac6fc897f028606c2afb317e697ae42288cb
Асатрян Оганнес <OSAsatryan.SBT@sberbank.ru>
2018-12-15 21:15:47 +0300

    moving main.dart to build release version

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:itunes_finder/presentation/main.dart';
+import 'package:itunes_finder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
